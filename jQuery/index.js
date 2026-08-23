ddab2e9315794ceb1f9efe05f397d64d976a186d
shanAweb <shanasif2002@gmail.com>
2026-08-24 04:19:07 +0500

    chore: added style classes using jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
   $("h1").css("color", "red");
   jQuery("h1").css("font-family", "Arial, sans-serif");
-  $("button").css("font-family", "Arial, sans-serif");
+  $("button").addClass("button-container btn");
+
   // event listener on jquery
   $("button").on("click", function () {
-    $("button").css("color", "blue");
+    $(this).css("color", "blue");
   });
 });
