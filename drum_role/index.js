8dcdcf4dd262ca73b5fbeb8dd29cb7de3a717154
shanAweb <shanasif2002@gmail.com>
2026-08-23 20:25:49 +0500

    chore: added keyboard events to play relevant drum sounds

--- drum_role/index.js
+++ drum_role/index.js
@@ -1,12 +1,49 @@
 var numberOFButtons = document.querySelectorAll(".drum").length;
 
+// mouse event listener
 for (var i = 0; i < numberOFButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", playTrack);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    playTrack(buttonInnerHTML);
+  });
+}
 
-  function playTrack() {
-    var audio = new Audio("sounds/1.mp3");
-    audio.play();
+// keyboard event listener
+document.addEventListener("keypress", function (event) {
+  playTrack(event.key);
+});
 
-    this.style.color = "brown";
+// function to play relevant sound
+function playTrack(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/5.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/6.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/7.mp3");
+      audio.play();
+      break;
   }
+  this.style.color = "brown";
 }
